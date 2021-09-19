8c2f838ab4432781b27812de5d3d4a4a0627cf3e
Kirill Filonchuk <kir.realtor@gmail.com>
2021-09-19 13:03:25 +0300

    ready

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,39 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import s from './ContactForm.module.css'
   
-class Form extends Component {
-    state = {
-     name: '',
-    number: '',
-    }
+function Form (props) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('')
     
- handleChange = event => {
+    
+ const handleChange = event => {
     const { name, value } = event.currentTarget;
-    // console.log(event.currentTarget);
-    // используем вычисляемые св-ва
-    this.setState({
-      [name]: value,
-    });
-    };
+    if (name === 'name') {
+      setName(value);
+      return
+    }
+    if (name === 'number') {
+      setNumber(value)
+    }   
+  };
     
-      handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    // const { name, number } = this.state;
-    // console.log(`
-    //   name: ${name}
-    //   number: ${number}
-    // `);
-    this.props.formSubmitHandler({ ...this.state });
-    this.reset();
+    
+    props.formSubmitHandler({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('')
   };
     
-    render() {
-        return (
-             <form onSubmit={this.handleSubmit} className={s.form}>
+    return (
+             <form onSubmit={handleSubmit} className={s.form}>
           Name
           
           <input
@@ -42,8 +39,8 @@ class Form extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
-            value={this.state.name}
-            onChange={this.handleChange}
+            value={name}
+            onChange={handleChange}
           />
          
           Number
@@ -54,14 +51,76 @@ class Form extends Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             required
-            value={this.state.number}
-            onChange={this.handleChange}
+            value={number}
+            onChange={handleChange}
           />
           
           <button type="submit" className={s.btnForm}>Add contact</button>
         </form>
         )
-    }
 }
 
 export default Form;
+
+// class Form extends Component {
+//     state = {
+//      name: '',
+//     number: '',
+//     }
+    
+//  handleChange = event => {
+//     const { name, value } = event.currentTarget;
+//     // console.log(event.currentTarget);
+//     // используем вычисляемые св-ва
+//     this.setState({
+//       [name]: value,
+//     });
+//     };
+    
+//       handleSubmit = e => {
+//     e.preventDefault();
+//     // const { name, number } = this.state;
+//     // console.log(`
+//     //   name: ${name}
+//     //   number: ${number}
+//     // `);
+//     this.props.formSubmitHandler({ ...this.state });
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+    
+//     render() {
+//         return (
+//              <form onSubmit={this.handleSubmit} className={s.form}>
+//           Name
+          
+//           <input
+//             type="text"
+//             name="name"
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//             required
+//             value={this.state.name}
+//             onChange={this.handleChange}
+//           />
+         
+//           Number
+         
+//           <input
+//             type="tel"
+//             name="number"
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+//             required
+//             value={this.state.number}
+//             onChange={this.handleChange}
+//           />
+          
+//           <button type="submit" className={s.btnForm}>Add contact</button>
+//         </form>
+//         )
+//     }
+// }
